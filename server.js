cd9fd540faeb02d6db133f983389b71f237a34e8
The Good Maxwell <maxwellraden@gmail.com>
2017-08-12 11:04:41 -0500

    server setup. creating blogPostRouter

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const blogPostsRouter = require('./blogPostsRouter');
+
+app.use(morgan('common'));
+app.use('/blog-posts', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`App is up on ${process.env.PORT || 8080}`);
+});
