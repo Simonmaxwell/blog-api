cee3774ee960d58e95c5a4e8acbdaa3788d97a0f
The Good Maxwell <maxwellraden@gmail.com>
2017-08-12 12:41:31 -0500

    created put function, considering pulling out required fields loop into it's own funtion, also considering re-evaluating my life choices.

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -24,5 +24,33 @@ router.post('/', jsonParser, (req, res) => {
     res.status(201).json(item);
 });
 
+router.put('/:id', jsonParser. (req, res) => {
+	const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`
+  			console.error(message);
+  			return res.status(400).send(message);
+		}
+	}
+	if (req.params.id !== req.body.id) {
+    	const message = (
+    		`Request path id (${req.params.id}) and request body id `
+    		`(${req.body.id}) must match`);
+    	console.error(message);
+   		return res.status(400).send(message);
+  }
+    console.log(`Updating blog post with id \`${req.params.id}\``);
+  	const updatedItem = BlogPosts.update({
+    	id: req.params.id,
+    	title: req.body.title,
+    	content: req.body.content,
+   		author: req.body.author,
+    	publishDate: req.body.publishDate
+ 	 });
+  	res.status(204).end();
+});
+
 module.exports = router;
 
